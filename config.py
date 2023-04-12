434a32aa77ce8d8d21394b89eec36464b504da77
Wenjun02 <130548556+Wenjun02@users.noreply.github.com>
2023-04-12 16:40:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.ccl1xmpotylf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanwenjun-employee"
+customregion = "us-east-1"
